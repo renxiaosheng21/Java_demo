b2f37243ddcfbe1f1478670b6e4a1bd5f1b0b573
renyongheng <renyongheng@wangdian.cn>
2020-09-14 22:40:57 +0800

    字符串分割 扩展

--- src/com/string/StringSplit.java
+++ src/com/string/StringSplit.java
@@ -1,5 +1,7 @@
 package com.string;
 
+import java.util.StringTokenizer;
+
 public class StringSplit {
     public static void main(String[] args) {
         String str = "Hello-World";
@@ -16,6 +18,14 @@ public class StringSplit {
         temp1 = str1.split(delimeter1);
         for(String x: temp1){
             System.out.println(x);
+        }
+        //在本例中用到String类的split方法，其实在字符串分割时可以调用StringTokenizer，实例化一个StringTokenizer的
+        //的对象，通过hashMoreToken()与nextToken进行判断并打印分割后的子字符串，具体代码如下
+        String str3 = "www.runoob.com";
+        StringTokenizer str2 = new StringTokenizer(str3,".");
+        //对str2遍历并打印字符串
+        while(str2.hasMoreTokens()){
+            System.out.println(str2.nextToken());
         }
     }
 }
